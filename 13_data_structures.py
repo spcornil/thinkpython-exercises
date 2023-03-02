ed9644cf7bc9f664be69443e9f0c6ad0d3eb7c2f
Sean Cornillie <sean.cornillie@gmail.com>
2023-03-02 14:04:12 -0700

    finished 13

--- 13_data_structures.py
+++ 13_data_structures.py
@@ -16,7 +16,7 @@ def clean_file(words):
                 wor = word.replacea(ele,'')
         #print(word)
     return word
-clean_file(fin)
+#clean_file(fin)
 
 #################
 # Exercise 13-2 #
@@ -132,4 +132,69 @@ def compare_lists(book, word_list):
         if bword not in wl:
             book_not_word.append(bword)
     return book_not_word
-print(compare_lists(rj, fin))  
+#print(compare_lists(rj, fin))
+
+#################
+# Exercise 13-5 #
+#################
+# Write a function named choose_from_hist that takes a histogram as defined in
+# and returns a random value from the histogram
+
+import random
+
+s = 'parrot'
+
+def histogram(s):
+    d = dict()
+    for i in s:
+        d[i] = 1 + d.get(i, 0)
+    return d
+#print(histogram(s))
+
+def choose_from_hist(s):
+    hist = histogram(s)
+    return random.choice(list(hist.values()))
+
+#print(choose_from_hist(s))
+
+#################
+# Exercise 13-6 #
+#################
+# Write a program that uses set subtraction to find words in the book that are not in the word list.
+
+# Note: already completed using sets in 13-4, so we'll just run the function with the new book and same word list
+emma = open('emma.txt')
+fin = open('words.txt')
+
+#print(compare_lists(emma, fin))
+
+#################
+# Exercise 13-7 #
+#################
+# Write a program that uses this algorithm to choose a random word from the book.
+import re
+import random
+
+#emma = open('emma.txt')
+rj = open('romeo_and_juliet.txt')
+
+START = '*** START'
+END = '*** END'
+    
+def random_word(book):
+    parsing = False
+    b = []
+    for line in book:
+        if line.startswith(END):
+            parsing = False
+        if parsing:
+            for word in line.split():
+                word = word.strip().lower()
+                word = re.sub('[^a-zA-Z]', '', word)    # Lots of weird characters so use regex instead of string punctuation
+                b.append(word)
+        if line.startswith(START):
+            parsing = True
+    #return b
+    return random.choice(list(set(b)))
+
+print(random_word(rj))
